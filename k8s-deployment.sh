6f89ead321b98f20171f08db489ab4e34d8e05dd
ranna95 <ranna.ramanath@gmail.com>
2022-05-23 16:27:51 +0530

    Update k8s-deployment.sh

--- k8s-deployment.sh
+++ k8s-deployment.sh
@@ -14,4 +14,5 @@ sed -i "s#replace#${imageName}#g" k8s_deployment_service.yaml
 #     kubectl -n default set image deploy ${deploymentName} ${containerName}=${imageName} --record=true
 # fi
 
+
 kubectl -n default apply -f k8s_deployment_service.yaml
